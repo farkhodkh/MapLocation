3dca606e780eb7056342060a9e6eedafb801a2a5
farkhod <986532qw@gmail.com>
2021-10-24 20:03:52 +0300

    Initial Commit

---
+++ MapLocation/MapLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapLocationApp.swift
+//  MapLocation
+//
+//  Created by Farkhod on 24.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MapLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
